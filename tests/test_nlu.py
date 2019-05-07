9516441efaa1b899e12a1d5f15874713f1f25682
smasyutin <smasyutin@lohika.com>
2019-05-07 15:24:39 -0700

    add NLU tests for 'greet', 'name' and 'bye'

--- tests/test_nlu.py
+++ tests/test_nlu.py
@@ -7,8 +7,16 @@ def test_nlu_interpreter():
     training_data = load_data("data/nlu_data.md")
     trainer = Trainer(config.load("nlu_config.yml"))
     interpreter = trainer.train(training_data)
-    test_interpreter_dir = trainer.persist("./tests/models")
-    parsing = interpreter.parse("hello")
 
-    assert parsing["intent"]["name"] == "greet"
+    test_interpreter_dir = trainer.persist("./tests/models")
     assert test_interpreter_dir
+
+    parsing = interpreter.parse("hello")
+    assert parsing["intent"]["name"] == "greet"
+
+    parsing = interpreter.parse("I am Rasa")
+    assert parsing["intent"]["name"] == "name"
+    assert parsing["entities"][0]["value"] == "Rasa".lower()
+
+    parsing = interpreter.parse("bye")
+    assert parsing["intent"]["name"] == "goodbye"
